7f01a4a19d96abb55a2b09f2b7aa8bdd55d6221e
hadis <hadisbeigi97@gmail.com>
2024-12-23 01:57:07 +0330

    hw-1/ex.html file

---
+++ hw-1/ex.js
@@ -0,0 +1,24 @@
+function renderList(n) {
+  let result = "";
+  for (let i = 1; i <= n; i++) {
+    result += `<li class="list-disc text-2xl text-blue-600">${i}</li>`;
+  }
+  const targetElement = document.querySelector("#books ul");
+  targetElement.innerHTML = result;
+}
+
+function validation(str) {
+  return !isNaN(parseInt(str));
+}
+function submit() {
+  const input = document.querySelector("#books input");
+  if (validation(input.value)) {
+    renderList(parseInt(input.value));
+  } else {
+    alert("please Enter a Number");
+  }
+  input.value = "";
+}
+
+const btn = document.querySelector("#submit-button");
+btn.addEventListener("click", submit);
